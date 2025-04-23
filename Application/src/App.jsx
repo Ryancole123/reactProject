74800f2a2805136b583f16bce4f8694143802a00
Ryancole123 <ryanandrewcole@hotmail.co.uk>
2025-04-23 12:34:20 +0100

    Sellers Link added and react-router-dom imported

--- Application/src/App.jsx
+++ Application/src/App.jsx
@@ -1,14 +1,24 @@
 
 import './App.css'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import Sellers from './Sellers'
 
 function App() {
 
 
   return (
     <>
-      <h3>test</h3>
-      <h3>Test 2</h3>
-      <h3>test3</h3>
+    <h2>System</h2>
+    
+
+    
+    <BrowserRouter>
+    <Link to="/sellers">Sellers</Link>
+      <Routes>
+        <Route path="/sellers" element={<Sellers/>}/>
+      </Routes>
+    </BrowserRouter>
+    
     </>
   )
 }
---
+++ Application/src/Sellers.jsx
@@ -0,0 +1,14 @@
+
+
+let Sellers = () =>{
+
+    return(
+        <>
+        <br/>
+        Sellers Placeholder Text
+        </>
+
+    )
+
+}
+export default Sellers
