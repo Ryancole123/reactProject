d49ebf78ba5a76904e1cac9c3652286867e33604
Ryancole123 <ryanandrewcole@hotmail.co.uk>
2025-04-23 14:32:31 +0100

    Nav + sellers partial

--- Application/src/Sellers.jsx
+++ Application/src/Sellers.jsx
@@ -6,19 +6,19 @@ let Sellers = () => {
 
     useEffect(() => {
     fetch("http://localhost:3000/seller")
-      .then((response) => response.json())
-      .then((data) => setSellers(data));
-  }, []);
+    .then((response) => response.json())
+    .then((data) => setSellers(data));
+}, []);
 
-  return(
-   <>
-   <h3>Sellers Placeholder Text</h3>
-   {
-    sellers.map((seller) => <div>{seller.firstName}</div>)
-   }
-  
-  
-  </>
-  )
+    return(
+        <>
+        <h3>Sellers Placeholder Text</h3>
+        {
+        sellers.map((seller) => <div>{seller.firstName}</div>)
+        }
+
+
+        </>
+    )
 };
 export default Sellers;
