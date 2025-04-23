657f7966bf64fc0e35a4b8fee481b1779527f83d
Thels <david.thelwell@dwp.gov.uk>
2025-04-23 19:00:47 +0100

    register 2

--- Application/src/App.jsx
+++ Application/src/App.jsx
@@ -4,11 +4,7 @@ import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import Sellers from './Sellers'
 import Buyers  from './buyers'
 import Properties from './properties'
-<<<<<<< HEAD
-import SellerForm from './SellerForm'
-=======
 import Register from './Register'
->>>>>>> ef5a1a7 (register)
 
 function App() {
 
@@ -35,11 +31,8 @@ function App() {
           <Route path="/sellers" element={<Sellers/>}/>
           <Route path="/properties" element={<Properties/>}/>
           <Route path="/buyers" element={<Buyers/>}/>
-<<<<<<< HEAD
           <Route path="/create-new-seller" element={<SellerForm/>}/>
-=======
           <Route path="/register" element={<Register/>}/>
->>>>>>> ef5a1a7 (register)
         </Routes>
     
 
