bd819400b1dc496e5babfa7e80146afe589c19ec
elyas.musa <elyas.musa@gmail.com>
2025-04-24 12:27:32 +0100

    propertyjsx new

--- Application/src/properties.jsx
+++ Application/src/properties.jsx
@@ -1,41 +1,42 @@
 import { useState,useEffect } from "react"
-// import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom"
 const Properties = () => {
     const [properties, setProperties] = useState([])
-
+        const hasGarden=(property)=>property ===1 
     useEffect(() => {
         fetch("http://localhost:3000/property")
             .then((response) => response.json())
             .then((data) => setProperties(data));
     }, []);
-
-    // let { sid } = useParams()
-
-    // let [properties, setProperties] = useState([])
-    // const processData = (ListProperties) => {
-
-    //     setProperties(ListProperties.filter((property) => property.sellerId == sid))
-    // }
-    // const processResponse = (response) => {
-    //     let responseObj1 = response.json()
-    //     responseObj1.then(processData)
-    // }
-    // const getProperties = () => {
-
-    //     let propertyObj1 = fetch("http://localhost:3000/property")
-    //     propertyObj1.then(processResponse)
-    // }
-
-    // getProperties()
     return (
-        <>
+        <div>
             <h2> List Of Properties of :</h2>
-            {
-                properties.map((property) => <div>{property.address}</div>)
-            }
-        </>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <td> Address </td>
+                        <td> Property Type </td>
+                        <td> Price </td>
+                        <td> No of Bedrooms </td>
+                        <td> No of Bathrooms </td>
+                        <td> Garden </td>
+                    </tr>
+                </thead>
+            
+            {properties.map((property) => 
+        
+                    <tr>
+                    <td> {property.address} </td>
+                        <td> {property.type} </td>
+                        <td> {property.price} </td>
+                        <td> {property.bedroom} </td>
+                        <td> {property.bathroom} </td>
+                        <td> {hasGarden(property.garden)?"Yes":"No"} </td>
+                    </tr>
+            )}
+            <Link to="/create-new-property"><input type="button" value="Add a new property"/></Link>
+            </table>
+        </div>
     )
 }
-
-
-export default Properties
+export default Properties;
